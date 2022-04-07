1bd750f5d7f05fdb093c50ee909a11e147305355
K Widmeyer <kerstyne.widmeyer@flemingcollege.ca>
2022-04-07 14:37:41 -0400

    team page

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+// Javascript used to call linkedin pages reference: https://github.com/KerstyneW/a3web/blob/main/js/javascript.js
+function LinkedinKW(){
+    window.open('https://www.linkedin.com/in/kerstyne%2Dwidmeyer%2D733010200');
+}
+function LinkedinJL(){
+    window.open('https://www.linkedin.com/in/jade%2Dlacsamana%2Da7224b131/')
+}
+function LinkinJK(){
+    window.open('https://www.linkedin.com/in/james-kwasnicki-481a20221/')
+}
